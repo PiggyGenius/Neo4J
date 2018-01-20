77c82a89ef09ec5f815124f45fa97271641bed96
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-20 10:11:49 +0100

    Fix instruction to run a query

--- db/run.sh
+++ db/run.sh
@@ -4,7 +4,7 @@ here=`pwd`
 mkdir -p "$here/data"
 mkdir -p "$here/logs"
 docker run \
-	--name neo4j \
+    --name neo4j \
     --publish=7474:7474 --publish=7687:7687 \
     --volume="$here/data":/data \
     --volume="$here/logs":/logs \
